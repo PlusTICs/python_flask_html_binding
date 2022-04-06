1476fa31157a44ebdfb0630166d1da61ab1dd391
Ricardo_Milos <ricardo98-avila@hotmail.com>
2022-04-06 17:06:29 -0500

    feat: login
    Se ha implementado un login

--- index.py
+++ index.py
@@ -4,23 +4,27 @@ import usuarios
 app = Flask(__name__);
 
 @app.route('/', methods=['GET'])
-def registro():
-    return render_template("index.html");
+def vista_login():
+    return render_template("login.html");
 
 @app.route('/', methods=['POST'])
+def login():
+    user = request.form['usuario'];
+    paswd = request.form['contrasenia'];
+    if(usuarios.loginSuperUser(user, paswd) == True):
+        return render_template("usuarios.html", name = usuarios.getUsuarios());
+    else:
+        return render_template("login.html");
+
+@app.route('/usuarios', methods=['GET'])
+def vista_registro_usuario():
+    return render_template("index.html");
+
+@app.route('/usuarios', methods=['POST'])
 def registrar_usuario():
     nombre = request.form['nombre'];
     usuarios.postRegistrarNuevoUsuario(nombre);
-    return render_template("usuarios.html");
-
-@app.route('/usuarios', methods=['GET'])
-def verUsuarios():
-    users = usuarios.getUsuarios();
-    list_users = "";
-    for i in range(0, len(users)):
-        list_users = list_users + users[i] + "\n"
-
-    return render_template("usuarios.html", name = list_users);
+    return render_template('usuarios.html', name = usuarios.getUsuarios());
 
 if __name__ == '__main__':
     app.run(debug=True, port=3000);
--- usuarios.py
+++ usuarios.py
@@ -1,7 +1,16 @@
 usuarios = ["Ervin", "Andres"];
 
+usuario = 'Andres';
+pasword = 'andres123';
+
 def getUsuarios():
     return usuarios;
 
 def postRegistrarNuevoUsuario(nombre):
     usuarios.append(nombre);
+
+def loginSuperUser(user, paswd):
+    login = False;
+    if(user == usuario and paswd == pasword):
+        login = True
+    return login;
